0762e1e1b615bb86c77abd8434362fcfcd900866
CallanBi <mcbihv@126.com>
2022-11-06 17:20:48 +0800

    fix: fix style issue in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.less';
 import React, { Suspense, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
